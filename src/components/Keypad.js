f6b1bc60958c19ee53d6642beda6bf3e6365db89
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-11-02 12:01:32 -0400

    completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad(){
+    function logInput(){
+        console.log("Entering password...")
+    }
+    return (
+        <input type="password" onChange={logInput}></input>
+    )
+}
+
+export default Keypad
