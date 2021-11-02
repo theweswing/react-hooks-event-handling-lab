0a42d4978fe18407a9d755752c3cf76f36957d54
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-11-02 12:01:26 -0400

    commit tree 7724bdc9009e4b226460a417734e981fe38ecde7

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function goodFocus(){
+        console.log("Good!")
+    }
+    function heyBlur(){
+        console.log("Hey! Eyes on me!")
+    } 
+    return (
+        <button onFocus={goodFocus} onBlur={heyBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
